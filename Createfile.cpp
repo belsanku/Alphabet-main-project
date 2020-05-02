26ae3c1e00e18438e0a4ad103c4de74dfaee708a
StepanovRoma <stepanovroma2019@gmail.com>
2020-05-02 13:02:28 +0700

    Rework func

--- Createfile.cpp
+++ Createfile.cpp
@@ -10,25 +10,36 @@
 #include <sstream>
 #include "supereraser.h"
 
+
+
 using namespace std;
+
+
 
 void CreateFile(string& x)
 {
-	string line;
-	string buf = "";
-	char fileName[80];
-	cout << "fileName: ";
-	cin >> fileName;
-	ifstream input(fileName);
+	string line, buf = "", path;
+	cout << "Enter the path: "<<endl;
+	getline(cin, path);
+	ifstream input(path);
+	if (!input.is_open()) // второй,  не очень юнит тест, скрытый
+	{
+		cout << "File can't be open!\n";
+		return;
+	}
 	ofstream output("SortFile.txt");
 	while (input) {
 		getline(input, line);
 		if (line.length())
 			buf += line + "\n";
 	}
+	cout << path; // первый скрытый юнит тест
 	supereraser(buf, x);
 	output << buf;
 	input.close();
+	path= "";
 	output.close();
+	buf = "";
+	line = "";
 	system("notepad SortFile");
 }
