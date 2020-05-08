9d87795b27b417f762c5f66a7e409047b8cacdd9
StepanovRoma <stepanovroma2019@gmail.com>
2020-05-08 21:19:55 +0700

    added choose func

---
+++ lib/choose.cpp
@@ -0,0 +1,40 @@
+#include "choose.h"
+#include "assistant.h"
+#include "stringin.h"
+#include "Createfile.h"
+#include <string>
+#include <iostream>
+using namespace std;
+void choose(string &l, string &x)
+{
+	getline(cin, x);
+	if (x == "help")
+	{
+		cout << endl;
+		assistant();
+		cout << endl;
+	}
+	else
+	{
+		size_t pos = x.find("string");
+		bool str = false;
+		if (pos != string::npos)
+			str = true;
+		if (str == true)
+		{
+			stringin(l, x);
+		}
+		else
+		{
+			pos = x.find("file");
+			bool file = false;
+			if (pos != string::npos)
+				file = true;
+			if (file == true)
+			{
+				CreateFile(x);
+			}
+		}
+	}
+	cout << endl;
+}
