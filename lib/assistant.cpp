85bc3796617e9f28d6d43aa4f067f3a48cd79b53
AlexGtTroll <www.syndicate@mail.ru>
2020-05-22 11:44:15 +0700

    code style updated

--- lib/assistant.cpp
+++ lib/assistant.cpp
@@ -3,14 +3,31 @@
 using namespace std;
 void assistant()
 {
-	cout << "The program for sorting text works as follows:" << endl <<
-		"-d  --  sorting without repetition" << endl;
-	cout << "-A -- case-sensitive text sorting. Uppercase letters are changed to lowercase." << endl;
-	cout << "-r -- conclusion from the end. Write to file from the end." << endl;
-	cout << "Parameters can be written in any order, example: -d -A -r, -r -A, -A -d;" << endl;
-	cout << "|| If none of the parameters is specified, then normal sorting will occur ||" << endl;
-	cout << "If you are working with a .txt file containing Russian letters, you must change the file encoding to ANSI" << endl;
-	cout << "Otherwise, at the exit you will receive an ancient elven language" << endl;
-	cout << "After you have specified the parameter, write what the program should work with" << endl;
-	cout << "Example: -d -A string - work with string. -d -A file - work with file" << endl;
+    cout << "The program for sorting text works as follows:" << endl
+         << "-d  --  sorting without repetition" << endl;
+    cout << "-A -- case-sensitive text sorting. Uppercase letters are changed "
+            "to "
+            "lowercase."
+         << endl;
+    cout << "-r -- conclusion from the end. Write to file from the end."
+         << endl;
+    cout << "Parameters can be written in any order, example: -d -A -r, -r -A, "
+            "-A -d;"
+         << endl;
+    cout << "|| If none of the parameters is specified, then normal sorting "
+            "will "
+            "occur ||"
+         << endl;
+    cout << "If you are working with a .txt file containing Russian letters, "
+            "you "
+            "must change the file encoding to ANSI"
+         << endl;
+    cout << "Otherwise, at the exit you will receive an ancient elven language"
+         << endl;
+    cout << "After you have specified the parameter, write what the program "
+            "should work with"
+         << endl;
+    cout << "Example: -d -A string - work with string. -d -A file - work with "
+            "file"
+         << endl;
 }
