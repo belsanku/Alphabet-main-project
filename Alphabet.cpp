25040eb43b2d670e6e75ca85ee164ed54ac29e5f
StepanovRoma <stepanovroma2019@gmail.com>
2020-05-24 16:23:05 +0700

    added old files

---
+++ Alphabet.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <locale>
+#include <set>
+#include <ctype.h>
+#include "printer.h"
+#include "textsortfull.h"
+#include "textsort.h"
+#include "parent.h"
+#include "symbols.h"
+#include "textchanger.h"
+#include "supereraser.h"
+#include "Createfile.h"
+#include "stringin.h"
+#include "assistant.h"
+#include "choose.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+    string x, l;
+    assistant();
+    cout << endl;
+    while (x != "exit") {
+        choose(l, x);
+    }
+}
