15ee088be7ca3d38750f5a78b1f792050f738ee1
StepanovRoma <stepanovroma2019@gmail.com>
2020-05-28 12:04:26 +0700

    fix includes

--- lib/textchanger.h
+++ lib/textchanger.h
@@ -1,22 +1,6 @@
-#include "Createfile.h"
-#include "assistant.h"
-#include "parent.h"
-#include "printer.h"
-#include "stringin.h"
-#include "supereraser.h"
-#include "symbols.h"
-#include "textsort.h"
-#include "textsortfull.h"
-#include <algorithm>
-#include <ctype.h>
-#include <fstream>
+#pragma once
 #include <iostream>
-#include <locale>
-#include <set>
-#include <sstream>
 #include <string>
 #include <vector>
-
 using namespace std;
-
 void textchanger(string& line, string& x);
